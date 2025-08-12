7fc46324059218671c26acb1a50afbf7ce898ba8
Iván Rodriguez <ivanrwcm25@gmail.com>
2025-08-12 00:23:40 -0300

    fixes de errs menores
    Signed-off-by: Iván Rodriguez <ivanrwcm25@gmail.com>

--- arch/x86-32/sources/idt_arch_x86.h
+++ arch/x86-32/sources/idt_arch_x86.h
@@ -7,19 +7,18 @@ void idt_arch_set_gate(int n, uintptr_t handler, uint8_t flags);
 
 typedef struct 
 {
-    uint16_t offset_low;
-    uint16_t selector;
-    uint8_t  type_attr;
-    uint32_t offset_high;
-    uint32_t zero;
+    uint16_t offset_low;   // Bits 0-15 del offset
+    uint16_t selector;     // Segment selector (GDT)
+    uint8_t  zero;         // Siempre 0 en x86
+    uint8_t  type_attr;    // P(1) | DPL(2) | 0(1) | GateType(4)
+    uint16_t offset_high;  // Bits 16-31 del offset
 } __attribute__((packed)) idt_entry_t;
 
-typedef struct 
-{
+// Estructura del puntero IDT para 'lidt'
+typedef struct {
     uint16_t limit;
-    uintptr_t base;
+    uint32_t base;         // Dirección lineal de la IDT
 } __attribute__((packed)) idt_ptr_t;
-
 
 // 32 bit
 
@@ -29,6 +28,6 @@ extern void isr_default(void);
 extern void isr_page_fault(void);
 extern void timer_stub(void);
 
-// Esta función le dice a la CPU que empiece a paginar configurando el directorio. -- 64 bit --
+// Esta función le dice a la CPU que empiece a paginar configurando el directorio. -- 32 bit --
 void paging_set_cpu();
 
