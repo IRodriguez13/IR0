2afde0e98a5c8fc209e391bc72b14376a810e756
Iván Rodriguez <ivanrwcm25@gmail.com>
2025-08-12 01:38:08 -0300

    [patch] añadido manejador simple de memoria.
    Signed-off-by: Iván Rodriguez <ivanrwcm25@gmail.com>
    Signed-off-by: Iván Ezequiel Rodriguez <ivanrwcm25@gmail.com>

--- arch/x_64/sources/Paging_x64.c
+++ arch/x_64/sources/Paging_x64.c
@@ -1,6 +1,7 @@
 #include "../../common/idt.h"
 #include "../../common/common_paging.h"
 #include "Paging_x64.h"
+#include <string.h>  // Para memset
 
 // Estructuras para paginación x86_64 (4 niveles)
 #define PML4_ENTRIES 512
@@ -21,6 +22,17 @@ __attribute__((aligned(PAGE_SIZE))) uint64_t PT[PT_ENTRIES];
 #define PAGE_USER    (1ULL << 2)
 #define PAGE_HUGE    (1ULL << 7)
 
+// Implementación local de memset para evitar dependencias
+static void* local_memset(void* ptr, int value, size_t size) 
+{
+    unsigned char* p = ptr;
+    while (size--) 
+    {
+        *p++ = (unsigned char)value;
+    }
+    return ptr;
+}
+
 void fill_page_table(uint64_t *table, uint64_t start_addr, uint64_t flags) 
 {
     for (uint64_t i = 0; i < PT_ENTRIES; i++) 
@@ -32,10 +44,10 @@ void fill_page_table(uint64_t *table, uint64_t start_addr, uint64_t flags)
 void init_paging_x64() 
 {
     // 1. Limpiar todas las entradas
-    memset(PML4, 0, PAGE_SIZE);
-    memset(PDPT, 0, PAGE_SIZE);
-    memset(PD, 0, PAGE_SIZE);
-    memset(PT, 0, PAGE_SIZE);
+    local_memset(PML4, 0, PAGE_SIZE);
+    local_memset(PDPT, 0, PAGE_SIZE);
+    local_memset(PD, 0, PAGE_SIZE);
+    local_memset(PT, 0, PAGE_SIZE);
 
     // 2. Mapeo identidad de los primeros 2MB (para código/tablas)
     // Nivel PML4
@@ -50,11 +62,18 @@ void init_paging_x64()
     // 3. Mapear memoria superior (ej: 0xFFFF800000000000)
     PML4[256] = ((uint64_t)PDPT) | PAGE_PRESENT | PAGE_WRITE;
     
-    // 4. Cargar CR3 con la dirección de PML4
+    // 4. Llamar a la función que carga CR3
+    paging_set_cpu_x64((uint64_t)PML4);
+}
+
+// NUEVA: Implementar la función que faltaba
+void paging_set_cpu_x64(uint64_t pml4_addr)
+{
+    // Cargar PML4 en CR3
     asm volatile(
         "mov %0, %%cr3"
         : 
-        : "r"((uint64_t)PML4)
+        : "r"(pml4_addr)
         : "memory"
     );
 }
