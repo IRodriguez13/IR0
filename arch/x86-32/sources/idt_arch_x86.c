12dfe4319996a9647df124a03eb745d6fc74cded
Iván Rodriguez <ivanrwcm25@gmail.com>
2025-08-12 23:59:44 -0300

    fixes
    Signed-off-by: Iván Rodriguez <ivanrwcm25@gmail.com>
    Signed-off-by: Iván Ezequiel Rodriguez <ivanrwcm25@gmail.com>

--- arch/x86-32/sources/idt_arch_x86.c
+++ arch/x86-32/sources/idt_arch_x86.c
@@ -1,30 +1,28 @@
-#include "idt_arch_x86.h"
+// arch/x86-32/sources/idt_arch_x86.c - CORREGIDO
+#include "../../common/idt.h"  // Solo incluir el header común
 
+// Array externo del IDT (definido en idt.c)
 extern idt_entry_t idt[256];
 
-
-extern idt_entry_t idt[256];
-
-void idt_arch_set_gate(int n, uintptr_t handler, uint8_t flags) {
-    idt[n].offset_low = handler & 0xFFFF;
-    idt[n].selector = 0x08;
-    idt[n].zero = 0;
-    idt[n].type_attr = flags;
-    idt[n].offset_high = (handler >> 16) & 0xFFFF;
+// Implementación específica para 32-bit
+void idt_arch_set_gate_32(int n, uintptr_t handler, uint8_t flags) 
+{
+    idt[n].offset_low = handler & 0xFFFF;           // Bits 0-15
+    idt[n].selector = 0x08;                         // Código del kernel
+    idt[n].zero = 0;                                // Siempre 0 en 32-bit
+    idt[n].type_attr = flags;                       // Tipo y privilegios
+    idt[n].offset_high = (handler >> 16) & 0xFFFF;  // Bits 16-31
 }
 
-// Implementación de 32 bit pero ahora portado.
-
+// Función de paginación para 32-bit
 void paging_set_cpu(uint32_t page_directory)
 {
-    // Le tengo que decir a la CPU cómo estoy paginando y que inicie la paginación
+    // Configurar CR3 con el directorio de páginas
     asm volatile("mov %0, %%cr3" ::"r"(page_directory));
 
-    // Le digo a la CPU que active el bit de paginación
+    // Activar el bit de paginación en CR0
     uint32_t cr0;
     asm volatile("mov %%cr0, %0" : "=r"(cr0));
-
-    cr0 |= 0x80000000;
-
+    cr0 |= 0x80000000;  // Activar bit 31 (PG - Paging)
     asm volatile("mov %0, %%cr0" ::"r"(cr0));
 }
