736f5f452e8cdbd8bb93c538e8a155c0067f3c1f
Iván Rodriguez <ivanrwcm25@gmail.com>
2025-08-21 16:24:50 -0300

    Subsistemas experimentales
    Signed-off-by: Iván Rodriguez <ivanrwcm25@gmail.com>

--- arch/x86-64/sources/arch_x64.c
+++ arch/x86-64/sources/arch_x64.c
@@ -25,6 +25,17 @@ void cpu_wait(void)
     __asm__ volatile("hlt");
 }
 
+// Implementaciones de funciones de I/O
+void outb(uint16_t port, uint8_t value) {
+    __asm__ volatile("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+uint8_t inb(uint16_t port) {
+    uint8_t value;
+    __asm__ volatile("inb %1, %0" : "=a"(value) : "Nd"(port));
+    return value;
+}
+
 void kmain_x64(void)
 {
     // Setup mínimo específico de x86-64
