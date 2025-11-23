baa04ca9b164c13614e87552a35b3c596bd25fa5
Iván Rodriguez <ivanrwcm25@gmail.com>
2025-11-23 09:50:22 -0300

    chore: remove WIP x86-32 and ARM-32 architecture directories
    - Delete arch-wip/x86-32 with all boot, IDT, and architecture-specific code
    - Delete arch-wip/arm-32 with all framebuffer, UART, and ARM-specific code
    - Clean up boot_x64.asm comments and formatting for consistency
    
    Signed-off-by: Iván Rodriguez <ivanrwcm25@gmail.com>

--- arch-wip/x86-32/sources/idt_arch_x86.h
+++
@@ -1,8 +0,0 @@
-// arch/x86-32/sources/idt_arch_x86.h - CORREGIDO
-#pragma once
-
-#include <stdint.h>
-
-// Declaraciones de funciones específicas para 32-bit
-void idt_arch_set_gate_32(int n, uintptr_t handler, uint8_t flags);
-void paging_set_cpu(uint32_t page_directory);
