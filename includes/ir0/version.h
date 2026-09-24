35454d92805a5567d1184f86a780c7b2fc78443e
Iván Ezequiel Rodriguez <ivanrwcm25@gmail.com>
2026-09-24 14:14:51 -0300

    chore(release): stamp IR0 0.0.1
    Remove the rc6 suffix so the kernel banner, uname output, build metadata, and final v0.0.1 tag identify the same release.
    
    Signed-off-by: Iván Ezequiel Rodriguez <ivanrwcm25@gmail.com>

--- includes/ir0/version.h
+++ includes/ir0/version.h
@@ -45,8 +45,8 @@
 #define IR0_VERSION_MAJOR 0
 #define IR0_VERSION_MINOR 0
 #define IR0_VERSION_PATCH 1
-#define IR0_VERSION_SUFFIX "-rc6"
-#define IR0_VERSION_STRING "0.0.1-rc6"
+#define IR0_VERSION_SUFFIX ""
+#define IR0_VERSION_STRING "0.0.1"
 
 /* Build information macros */
 /* If passed from Makefile, use those (real data), otherwise use compiler defaults */
